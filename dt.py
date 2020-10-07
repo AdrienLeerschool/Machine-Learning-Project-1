f744bc34afd273593288b72961c0acade84dd3e5
Adrien <adrien.leerschool@student.ulg.ac.be>
2020-10-07 23:21:23 +0200

    Final Q1 + plots + tree + averages

--- dt.py
+++ dt.py
@@ -13,67 +13,84 @@ from sklearn.tree import DecisionTreeClassifier
 from data import make_data1, make_data2
 from plot import plot_boundary
 from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
 
-from sklearn.datasets import load_iris
 from sklearn import tree
 import graphviz 
+
+
+def make_model(size_ts = 10000, size_ls = 250, data_set = 1, graph = False, depth = None):
+    
+    if data_set == 1 :
+        [X_train, y_train, X_test, y_test] = make_data1(size_ts,size_ls,0,None)
+    else:
+        [X_train, y_train, X_test, y_test] = make_data2(size_ts,size_ls,0,None)
+
+    clf = DecisionTreeClassifier(max_depth=depth)
+    clf = clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    
+    if graph:
+        if depth == None :
+            plot_boundary(fname="figures/data"+str(data_set)+"_depthNone",fitted_estimator=clf,
+                          X=X_test,y=y_pred,title="data set "+str(data_set)+" with depth = None")
+            
+            tree.plot_tree(clf)
+            dot_data = tree.export_graphviz(clf, out_file=None)
+            graph = graphviz.Source(dot_data)
+            graph.render("figures/tree_data"+str(data_set)+"_depthNone")
+        
+        else:
+            plot_boundary(fname="figures/data"+str(data_set)+"_depth"+str(depth),fitted_estimator=clf,
+                          X=X_test,y=y_pred,title="data set "+str(data_set)+" with depth = "+str(depth))
+                   
+    return accuracy_score(y_test, y_pred)
 
 
 
 if __name__ == "__main__":
     
     depths = [1, 2, 4, 8, 0]
-    [X_train, y_train, X_test, y_test] = make_data1(10000,250,0,None)
-    print("Data set 1 :")
+    scores = []
+    nb_iter = 4
     
-    for i in depths:
-        if i == 0 :
-            clf = DecisionTreeClassifier(max_depth=None)
-        else:
-            clf = DecisionTreeClassifier(max_depth=i)
-        clf = clf.fit(X_train, y_train)
-        y_pred = clf.predict(X_test)
-        
-        
-        if i == 0 :
-            print("Accuracy for depth = None : "+ str(accuracy_score(y_test, y_pred)))
-            plot_boundary(fname="figures/data1_depthNone",fitted_estimator=clf,
-                          X=X_test,y=y_pred,title="First data set with depth = None")
-            
-            tree.plot_tree(clf)
-            dot_data = tree.export_graphviz(clf, out_file=None)
-            graph = graphviz.Source(dot_data)
-            graph.render("figures/tree_data1_depthNone")
-            
-        else:
-            print("Accuracy for depth = "+str(i)+" : "+ str(accuracy_score(y_test, y_pred)))
-            plot_boundary(fname="figures/data1_depth"+str(i),fitted_estimator=clf,
-                          X=X_test,y=y_pred,title="First data set with depth = "+str(i))
-        
+    # First data set
+    print("First data set :\n")
     
-    
-    [X_train, y_train, X_test, y_test] = make_data2(10000,250,0,None)
-    print("\nData set 2 :")
-    for i in depths:
+    for i in depths :
+        scores.clear()
+        for j in range(nb_iter) :
+            if i == 0 :
+                scores.append(make_model(depth=None))
+            else:
+                scores.append(make_model(depth=i))
+                
         if i == 0 :
-            clf = DecisionTreeClassifier(max_depth=None)
+            scores.append(make_model(depth=None, graph=True))
+            print("Average accuracy for unconstrained depth : "+ str(np.mean(scores)) +
+                  ",\nstandard deviation : "+str(np.std(scores)))
         else:
-            clf = DecisionTreeClassifier(max_depth=i)
-        clf = clf.fit(X_train, y_train)
-        y_pred = clf.predict(X_test)
+            scores.append(make_model(depth=i))
+            print("Average accuracy for max_depth = "+str(i)+" : "+ str(np.mean(scores))+
+                  ",\nstandard deviation : "+str(np.std(scores)))
+      
+    # Second data set
+    print("\n\nSecond data set :\n")
         
+    for i in depths :
+        scores.clear()
+        for j in range(nb_iter) :
+            if i == 0 :
+                scores.append(make_model(depth=None, data_set=2))
+            else:
+                scores.append(make_model(depth=i, data_set=2))
+                
         if i == 0 :
-            print("Accuracy for depth = None : "+ str(accuracy_score(y_test, y_pred)))
-            plot_boundary(fname="figures/data2_depthNone",fitted_estimator=clf,
-                          X=X_test,y=y_pred,title="Second data set with depth = None")
-            
-            tree.plot_tree(clf)
-            dot_data = tree.export_graphviz(clf, out_file=None)
-            graph = graphviz.Source(dot_data)
-            graph.render("figures/tree_data2_depthNone")
-            
+            scores.append(make_model(depth=None, graph=True, data_set=2))
+            print("Average accuracy for unconstrained depth : "+ str(np.mean(scores)) +
+                  ",\nstandard deviation : "+str(np.std(scores)))
         else:
-            print("Accuracy for depth = "+str(i)+" : "+ str(accuracy_score(y_test, y_pred)))
-            plot_boundary(fname="figures/data2_depth"+str(i),fitted_estimator=clf,
-                          X=X_test,y=y_pred,title="Second data set with depth = "+str(i))
+            scores.append(make_model(depth=i, data_set=2))
+            print("Average accuracy for max_depth = "+str(i)+" : "+ str(np.mean(scores))+
+                  ",\nstandard deviation : "+str(np.std(scores)))
+    
+
